0bc7b9b7e608510d20a7f0e452988bebc0abd194
Mike Monteith <mike@mikemonteith.com>
2016-09-15 16:56:12 +0100

    remove modals from list-input

--- src/ui/components/list-input.js
+++ src/ui/components/list-input.js
@@ -1,33 +1,33 @@
 
-import _ from '../i18n';
-
 import React from 'react';
 import StyleSheet from '../styles';
 import { View, Text, TouchableHighlight} from 'react-native';
 
-import Dialog from './dialog';
-import Picker from './picker';
 import Button from './button';
 import TextInput from './text-input';
+import Popup from './popup';
 
 export default class ListInput extends React.Component {
 
-  onPress = () => {
-    if(!this.props.modalProvider) return;
-    const modalProvider = this.props.modalProvider();
+  constructor() {
+    super();
 
-    modalProvider.showModal(<Dialog popup={true} onClose={() => modalProvider.hideModal()} style={StyleSheet.dialog.optionsMenu}>
-      {this.props.children.map((x, i) => React.cloneElement(x, { key: i, onPress: () => this.onPressChild(modalProvider, x) }))}
-    </Dialog>)
+    this.state = {
+      showPopup: false,
+    };
+  }
+
+  onPress = () => {
+    this.setState({showPopup: true});
   };
 
-  onPressChild = (modalProvider, child) => {
-    modalProvider.hideModal();
-    if(this.props.onChange) this.props.onChange(child.props.value);
+  onPressChild = (value) => {
+    this.setState({showPopup: false});
+    this.props.onChange && this.props.onChange(value);
   };
 
   render() {
-    const {onChange, rightBar, value, placeholder, children, style, ...props} = this.props;
+    const {rightBar, value, placeholder, style, ...props} = this.props;
 
     const defaultTextInput = StyleSheet.textInputs.default || {};
     const textInput = this.props.type ? StyleSheet.textInputs[this.props.type] || defaultTextInput : defaultTextInput;
@@ -35,28 +35,60 @@ export default class ListInput extends React.Component {
     const selectedChild = value ? this.props.children.find(x => x.props.value === value) : null;
 
     return (
-      <TouchableHighlight onPress={this.onPress}
-                    activeOpacity={'activeOpacity' in textInput ? textInput.activeOpacity : defaultTextInput.activeOpacity}
-                    underlayColor={'underlayColor' in textInput ? textInput.underlayColor : defaultTextInput.underlayColor}>
-        <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
-          <TextInput style={[{flex:1}, style]} view={
-            <Text style={[
-              StyleSheet.text,
-              defaultTextInput.textStyle,
-              textInput.textStyle,
-              defaultTextInput.staticTextStyle,
-              textInput.staticTextStyle,
-              this.props.textStyle,
-              !selectedChild && { color: this.props.placeholderTextColor || textInput.placeholderTextColor || defaultTextInput.placeholderTextColor }
-            ]}>
-              {!!selectedChild ? selectedChild.props.text : placeholder}
-            </Text>
-          } {...props} />
-          <View style={[defaultTextInput.barStyle, textInput.barStyle]}>{rightBar}</View>
-        </View>
-      </TouchableHighlight>
+      <View>
+        <Popup
+          visible={this.state.showPopup}
+          onClose={() => this.setState({showPopup: false})}
+          style={StyleSheet.dialog.optionsMenu}
+        >
+          {this.props.children.map((child, i) => {
+            return React.cloneElement(
+              child,
+              {
+                key: i,
+                onPress: () => this.onPressChild(child.props.value),
+                ...child.props,
+              }
+            );
+          })}
+        </Popup>
+        <TouchableHighlight
+          onPress={this.onPress}
+          activeOpacity={'activeOpacity' in textInput ? textInput.activeOpacity : defaultTextInput.activeOpacity}
+          underlayColor={'underlayColor' in textInput ? textInput.underlayColor : defaultTextInput.underlayColor}
+        >
+          <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+            <TextInput style={[{flex:1}, style]} view={
+              <Text style={[
+                StyleSheet.text,
+                defaultTextInput.textStyle,
+                textInput.textStyle,
+                defaultTextInput.staticTextStyle,
+                textInput.staticTextStyle,
+                this.props.textStyle,
+                !selectedChild && { color: this.props.placeholderTextColor || textInput.placeholderTextColor || defaultTextInput.placeholderTextColor }
+              ]}>
+                {selectedChild ? selectedChild.props.text : placeholder}
+              </Text>
+            } {...props} />
+            <View style={[defaultTextInput.barStyle, textInput.barStyle]}>{rightBar}</View>
+          </View>
+        </TouchableHighlight>
+      </View>
     );
   }
+}
+
+ListInput.propTypes = {
+  onChange: React.PropTypes.func,
+  rightBar: React.PropTypes.node,
+  value: React.PropTypes.string,
+  placeholder: React.PropTypes.string,
+  children: React.PropTypes.arrayOf(React.PropTypes.node),
+  style: View.propTypes.style,
+  textStyle: Text.propTypes.style,
+  type: React.PropTypes.string.isRequired,
+  placeholderTextColor: React.PropTypes.string,
 };
 
 ListInput.Item = class ListInputItem extends React.Component {
@@ -65,4 +97,9 @@ ListInput.Item = class ListInputItem extends React.Component {
       <Button type="alertVertical" text={this.props.text} onPress={this.props.onPress} />
     );
   }
+};
+
+ListInput.Item.propTypes = {
+  onPress: React.PropTypes.func,
+  text: React.PropTypes.string.isRequired,
 };
