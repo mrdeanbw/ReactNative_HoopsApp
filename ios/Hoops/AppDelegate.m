853bebc57f79df30bf1959e5ab40f32a51cb9a20
Mike Monteith <mike@mikemonteith.com>
2016-10-26 12:01:08 +0100

    Revert AppDelegate.m jsLocation change

--- ios/Hoops/AppDelegate.m
+++ ios/Hoops/AppDelegate.m
@@ -38,8 +38,7 @@
 
   NSURL *jsCodeLocation;
 
-  //jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.64:8081/index.ios.bundle"];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Hoops"
