6a081e9caca1e8d0fe9d8ea37e307744c3115979
Mike Monteith <mike@mikemonteith.com>
2016-10-31 17:05:28 +0000

    Make all device locales use english translations

--- src/ui/i18n/index.js
+++ src/ui/i18n/index.js
@@ -2,5 +2,7 @@
 import i18n from 'react-native-i18n';
 export default i18n.t.bind(i18n);
 
+i18n.fallbacks = true;
+
 import * as enUS from './strings/en-us';
-i18n.translations['en-US'] = enUS;
+i18n.translations['en'] = enUS;
