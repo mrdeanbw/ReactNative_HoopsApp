f7150aacfa2b11405492cefb48e15328764312bd
Lee Solway <lee@digitalanvil.co.uk>
2017-03-15 12:32:40 +0000

    Hide recurring fields

--- src/windows/create-event.js
+++ src/windows/create-event.js
@@ -390,33 +390,35 @@ export default class CreateEvent extends React.Component {
                 <Button type="roundedGrey" active={this.state.eventDetails.recurring === false} text={_('oneTime')} onPress={() => this.setEventData({recurring: false})} style={{width: 110}}/>
               </View>
 
-              <View style={[StyleSheet.doubleMarginTop, {flex:1, flexDirection: 'row'}]}>
-                <Text style={[{flex: 1, marginTop: 14, marginRight: 25, color: StyleSheet.colors.pink, fontSize: 13}]}>
-                  {_('recurEvery').toUpperCase()}
-                </Text>
-                <TextInput
-                  ref="recurringValue"
-                  type="flat"
-                  keyboardType="numeric"
-                  value={this.state.eventDetails.recurringValue.toString()}
-                  onChangeText={recurringValue => this.setEventData({recurringValue})}
-                  style={{flex: 1, marginRight: 25}}
-                  disabled={!this.state.eventDetails.recurring}
-                  onFocus={() => {
-                    this.scrollToInput('scrollView2', this.refs.recurringValue)
-                  }}
-                />
-                <ListInput
-                  type="flat"
-                  value={this.state.eventDetails.recurringType}
-                  containerStyle={{flex:1}}
-                  onChange={(recurringType) => this.setEventData({recurringType})}
-                  disabled={!this.state.eventDetails.recurring}
-                >
-                  <ListInput.Item text={_('days')} value="d" />
-                  <ListInput.Item text={_('weeks')} value="w" />
-                </ListInput>
-              </View>
+              {this.state.eventDetails.recurring && (
+                <View style={[StyleSheet.doubleMarginTop, {flex:1, flexDirection: 'row'}]}>
+                  <Text style={[{flex: 1, marginTop: 14, marginRight: 25, color: StyleSheet.colors.pink, fontSize: 13}]}>
+                    {_('recurEvery').toUpperCase()}
+                  </Text>
+                  <TextInput
+                    ref="recurringValue"
+                    type="flat"
+                    keyboardType="numeric"
+                    value={this.state.eventDetails.recurringValue.toString()}
+                    onChangeText={recurringValue => this.setEventData({recurringValue})}
+                    style={{flex: 1, marginRight: 25}}
+                    disabled={!this.state.eventDetails.recurring}
+                    onFocus={() => {
+                      this.scrollToInput('scrollView2', this.refs.recurringValue)
+                    }}
+                  />
+                  <ListInput
+                    type="flat"
+                    value={this.state.eventDetails.recurringType}
+                    containerStyle={{flex:1}}
+                    onChange={(recurringType) => this.setEventData({recurringType})}
+                    disabled={!this.state.eventDetails.recurring}
+                  >
+                    <ListInput.Item text={_('days')} value="d" />
+                    <ListInput.Item text={_('weeks')} value="w" />
+                  </ListInput>
+                </View>
+              )}
 
               <View style={StyleSheet.halfMarginTop}>
                 <TextInput
