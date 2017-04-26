4adbb0ddec500d70a6eefdde62d72a5a49ecd25b
Lee Solway <lee@digitalanvil.co.uk>
2017-04-26 20:47:11 +0100

    Added scroll view to the wallet

--- src/windows/wallet.js
+++ src/windows/wallet.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {ScrollView, View, Text} from 'react-native'
 
 import _ from '../i18n'
 import {Header, LoadingAlert} from '../components'
@@ -22,7 +22,7 @@ class Wallet extends Component {
           <LoadingAlert visible={this.props.isLoading} />
 
           {account && account.accountNumber ? (
-            <View>
+            <ScrollView>
               <Text style={titleStyle}>{_('firstName')}</Text>
               <Text style={detailStyle}>{account.firstName}</Text>
 
@@ -46,7 +46,7 @@ class Wallet extends Component {
 
               <Text style={titleStyle}>{_('postcode')}</Text>
               <Text style={detailStyle}>{account.postcode}</Text>
-            </View>
+            </ScrollView>
           ) : (
             <Text style={StyleSheet.payments.noCardsText}>{_('noAccount')}</Text>
           )}
