faeb1134ca00a7aab40fc8ef8829684271d4c87d
Rafal Kaczynsky <rafalkaczynsky@Rafals-MacBook-Pro.local>
2017-05-24 10:30:33 +0100

    fixed noFutureDates Validation

--- src/config/validation.js
+++ src/config/validation.js
@@ -3,7 +3,7 @@ const validation = {
   email: value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined,
   maxChars20: (value) => !(value.length >= 20) ? undefined : 'Must be 20 characters or less',
   minChars6: (value, number) => !(value.length <= number) ? undefined : 'Password  must be at least 6 characters.',
-  noFutureDates: (date, today = new Date()) => (date >= today) ? 'Invalid date of birth' : false,
+  noFutureDates: (value) => (value >=  new Date()) ? 'Invalid date of birth' : false,
 }
 
 export default validation
