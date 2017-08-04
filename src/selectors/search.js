82a2b530959b2899e3f3b4ae838f59802d0ca8b7
Lee Solway <lee@digitalanvil.co.uk>
2017-08-04 03:40:28 +0100

    basic search selectors

--- src/selectors/search.js
+++ src/selectors/search.js
@@ -1,1 +1,7 @@
 export const searchNearbyEventsSelector = (state) => state.search.nearby
+
+export const searchGeneralEventsSelector = (state) => state.search.general.eventIds
+
+export const searchGeneralUsersSelector = (state) => state.search.general.userIds
+
+export const searchEventsSelector = (state) => state.search.eventIds
