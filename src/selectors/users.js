74a21e7d116b9d7d88fa0e22acf7fe6959dc1487
Lee Solway <lee@digitalanvil.co.uk>
2017-08-04 03:40:18 +0100

    user selector by id

--- src/selectors/users.js
+++ src/selectors/users.js
@@ -1,1 +1,8 @@
+import {createSelector} from 'reselect'
+
 export const usersSelector = (state) => state.users.usersById
+
+export const userByIdSelector = createSelector(
+  [usersSelector, (state, userId) => userId],
+  (users, userId) => users[userId]
+)
